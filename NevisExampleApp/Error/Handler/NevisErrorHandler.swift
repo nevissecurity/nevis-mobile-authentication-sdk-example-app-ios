f696287a25b96bb1fa79e8f2690b462d66b29aa8
Tamas Toth <tamas.toth@nevis.net>
2025-03-04 11:07:58 +0100

    NEVISACCESSAPP-6483: Clarify technical errors

--- NevisExampleApp/Error/Handler/NevisErrorHandler.swift
+++ NevisExampleApp/Error/Handler/NevisErrorHandler.swift
@@ -41,6 +41,11 @@ extension NevisErrorHandler: ErrorHandler {
 
 		logger.sdk("Operation error occurred: %@", .red, .debug, operationError.underlyingError.localizedDescription)
 
+		// The underlying error is either an `AppError` or a `MobileAuthenticationClientError`.
+		// As this is an example app, we are directly showing the technical error occurring. Be aware that this is not
+		// to be considered best practice. Your own production app should handle the errors in a more appropriate manner
+		// such as providing translations for all your supported languages as well as simplifying the error message
+		// presented to the end-user in a way non-technical adverse people can understand and act upon them.
 		coordinator.navigateToResult(with: .failure(operation: operationError.operation,
 		                                            description: operationError.underlyingError.localizedDescription))
 		chain.handle(error: error)
